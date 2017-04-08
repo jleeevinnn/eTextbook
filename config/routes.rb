a557599373ee80108a6a7c88a1b6d1e30727cdaa
jleeevinnn <lo0fworld@hotmail.com>
2017-04-08 12:20:19 -0700

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  default_url_options :host => "http://etextbook.herokuapp.com"
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
   resources :sections, only: [:new, :create, :index, :destroy]
   resources :chapters, only: [:index]
   root 'chapters#index'
+  default_url_options :host => "http://etextbook.herokuapp.co
 end
