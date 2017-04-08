6f3f324c957a17f355fb7abc4d6eb88c4cdd398c
jleeevinnn <lo0fworld@hotmail.com>
2017-04-08 12:27:24 -0700

    fuck

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_filter :mailer_set_url_options
   protect_from_forgery with: :exception
 
-
+ def mailer_set_url_options
+    ActionMailer::Base.default_url_options[:host] = request.host_with_port
+  end
 end
