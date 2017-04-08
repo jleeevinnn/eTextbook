f4c8944a3a0cfc61f918266b0377f59b0081057d
jleeevinnn <lo0fworld@hotmail.com>
2017-04-08 12:36:28 -0700

    Done

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,9 +1,9 @@
-ActionMailer::Base.smtp_settings = { 
-  :address              => 'smtp.gmail.com',
-  :port                 => 587,
-  :domain               => 'gmail.com',
-  :user_name            => 'leagueoffactions@gmail.com',
-  :password             => 'banditRebel123',
-  :authentication       => 'plain',
-  :enable_starttls_auto => true
-  }
+config.action_mailer.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :domain => 'etextbook.herokuapp.com:80',
+      :user_name => "leagueoffactions@gmail.com",
+      :password => "banditRebel123",
+      :authentication => :plain,
+      :enable_starttls_auto => true
+}
