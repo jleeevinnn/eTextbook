6570e51b31511f4ba8b187e3bbc34fb57240bbf7
jleeevinnn <lo0fworld@hotmail.com>
2017-04-08 12:38:14 -0700

    done

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,7 +1,7 @@
 config.action_mailer.smtp_settings = {
       :address => "smtp.gmail.com",
       :port => 587,
-      :domain => 'etextbook.herokuapp.com:80',
+      :domain => 'gmail.com',
       :user_name => "leagueoffactions@gmail.com",
       :password => "banditRebel123",
       :authentication => :plain,
