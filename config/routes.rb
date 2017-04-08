ac5151e66f7fdb1916847d00a1746a134489c1ae
jleeevinnn <lo0fworld@hotmail.com>
2017-04-08 13:03:01 -0700

    Added /sections/[:id] get route

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :sections, only: [:new, :create, :index, :destroy]
   resources :chapters, only: [:index]
+get '/sections/:id', to: 'sections#show'
   default_url_options :host => "localhost:3000"
 end
